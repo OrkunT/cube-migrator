439b3c259e8b5b1b0f41ba954348ef1e7aa74973
OrkunT <34773848+OrkunT@users.noreply.github.com>
2024-05-26 02:49:33 +0300

    Update webpack.config.js
    add dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,18 @@ module.exports = {
       "util": require.resolve("util/"),
       "buffer": require.resolve("buffer/"),
       "stream": require.resolve("stream-browserify"),
-      "crypto": require.resolve("crypto-browserify")
+      "crypto": require.resolve("crypto-browserify"),
+      "aws4": require.resolve("aws4"),
+      "mongodb-client-encryption": require.resolve("mongodb-client-encryption"),
+      "http": require.resolve("http"),
+      "url": require.resolve("url"),
+      "querystring": require.resolve("querystring"),
+      "os": require.resolve("os"),
+      "zlib": require.resolve("zlib"),
+      "fs": require.resolve("fs/"),
+      "path": require.resolve("path/"),
+      "dns": require.resolve("dns/"),
+      "assert": require.resolve("assert/"),
+      "module": require.resolve("module/")
     },
 };
