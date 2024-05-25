66c69f48ae7f7cb8469b08d57ebe39050a91fecb
OrkunT <34773848+OrkunT@users.noreply.github.com>
2024-05-26 01:14:43 +0300

    Update webpack.config.js
    resolve fallback required for polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    fallback: {
+      "util": require.resolve("util/"),
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify"),
+      "crypto": require.resolve("crypto-browserify")
+    },
 };
