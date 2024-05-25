a24d03fbd7c3641110361b75191fa34514b23944
OrkunT <34773848+OrkunT@users.noreply.github.com>
2024-05-26 01:01:00 +0300

    Update webpack.config.js
    better indexing easier to add folder for different dbs

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './mongodb/index.js',
+  entry: './index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
